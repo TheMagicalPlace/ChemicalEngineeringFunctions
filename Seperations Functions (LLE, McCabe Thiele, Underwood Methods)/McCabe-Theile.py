7195c710c967b855c20e051e8e2d196b5628cd70
TheMagicalPlace <chris621@msu.edu>
2019-07-19 22:26:50 -0400

    partial implimentation of multiple feed lines for mccabe-thiele

--- Seperations Functions (LLE, McCabe Thiele, Underwood Methods)/McCabe-Theile.py
+++ Seperations Functions (LLE, McCabe Thiele, Underwood Methods)/McCabe-Theile.py
@@ -7,83 +7,115 @@ composition.
 
 '''
 
-from numpy import array,interp,append,insert
+from numpy import array,interp,append,delete,where
 from matplotlib import pyplot as plot
 
 class MTMethod():
+    # TODO - impliment partial condensers -- refer to seps book for information
 
-    def __init__(self,xdata,ydata,dfrac,bfrac,feed,feed_composition,lfrac_feed,reflux_ratio):
-        self.xydata = array([xdata,ydata])
+    def __init__(self,xdata,ydata,dfrac,bfrac,feed,feed_composition,liquid_frac_feed,reflux_ratio):
+        self.xydata = array([xdata, ydata])
         self.R = reflux_ratio
-        self.outfracs = [dfrac,bfrac]
-        self.flows = feed*feed_composition
-        self.feed_comp = feed_composition
-        self.q = lfrac_feed #
-        self.feed= feed
-        self.distillate_fraction = dfrac
-        self.bottoms_fraction = bfrac
-        self.distillate = ((feed_composition-bfrac)/(dfrac-bfrac))*feed
-        self.bottoms = feed-self.distillate
+        self.feed_line = []
+        # TODO add a way to deal with sidestreams (incl. all liquid, all vapor, etc.)
+        # for this, consider requiring an order for sidestreams and feeds,
+        # i.e. [(distillate),Feed1,side1,side2,feed2...(bottoms)]
+
+        self.feed_flow_composition_and_state = [[a, b, c] for a, b, c in zip(feed,feed_composition,liquid_frac_feed)]
+
+        self.db_fracs = [dfrac,bfrac]
+        self.distillate = sum([((feed_composition-bfrac)/(dfrac-bfrac))*feed
+                              for feed,feed_composition,_ in self.feed_flow_composition_and_state])
+        self.bottoms = sum(feed)-self.distillate
+
         self.line45 = array([x/100 for x in range(1,101)])
+        self.mass_balance()
+        self.find_feed_lines()
 
     def tray_finder(self):
-
+        #TODO update this to work with how the rest is structured
         tray_compositions = array([[self.distillate_fraction,self.distillate_fraction]])
-        print(tray_compositions)
-        while tray_compositions[-1,0] > self.bottoms_fraction:
-            if tray_compositions[-1,0] > self.intersectt[0]:
-                print(tray_compositions)
+
+
+        while tray_compositions[-1,1] > self.bottoms_fraction:
+            for fcomp in feed_composition:
+                if tray_compositions[-1,0] > self.intersectt[0]:
+                    tray_compositions = append(tray_compositions, array([[interp(tray_compositions[-1, 1],
+                                                                                 self.xydata[1], self.xydata[0]),
+                                                                          tray_compositions[-1, 1]]]), axis=0)
+                    plot.plot([tray_compositions[-2, 0], tray_compositions[-1, 0]],
+                             [tray_compositions[-1, 1], tray_compositions[-1, 1]])
+
+                    tray_compositions = append(tray_compositions, array([[tray_compositions[-1, 0],
+                                               interp(tray_compositions[-1, 0],self.line45,self.stripping_line)]]), axis=0)
+
+
+                    plot.plot([tray_compositions[-2, 0], tray_compositions[-2, 0]],
+                              [tray_compositions[-1, 1], tray_compositions[-2, 1]])
+            else:
+
                 tray_compositions = append(tray_compositions, array([[interp(tray_compositions[-1, 1],
                                                                              self.xydata[1], self.xydata[0]),
                                                                       tray_compositions[-1, 1]]]), axis=0)
-                plot.plot([tray_compositions[-1, 0], tray_compositions[-2, 0]],
+                plot.plot([tray_compositions[-2, 0], tray_compositions[-1, 0]],
                          [tray_compositions[-1, 1], tray_compositions[-1, 1]])
 
                 tray_compositions = append(tray_compositions, array([[tray_compositions[-1, 0],
-                                           interp(tray_compositions[-1, 0],self.line45,self.stripping_line)]]), axis=0)
+                                           interp(tray_compositions[-1, 0],self.line45,self.rectifying_line)]]), axis=0)
 
+                plot.plot([tray_compositions[-2, 0], tray_compositions[-2, 0]],
+                          [tray_compositions[-1, 1], tray_compositions[-2, 1]])
 
-                plot.plot([tray_compositions[-1, 0], tray_compositions[-1, 0]],
-                          [tray_compositions[-2, 1], tray_compositions[-1, 1]])
-            else:
-                return
+        print(tray_compositions,len(tray_compositions)//2)
 
+    def mass_balance(self):
 
+        self.L = [self.R*self.distillate] # Liquid outflow from condenser (assumed saturated liquid)
+        self.V = [self.L[0]+self.distillate] # Vapor entering into condenser
+        for feed, _, q in self.feed_flow_composition_and_state:
+            self.L.append(sum(self.L)+feed*q)
+            self.V.append(sum(self.V)-feed*(1-q))
 
     def find_operating_lines(self):
 
-        self.L0 = self.R*self.distillate
-        self.V = self.L0+self.distillate
-        self.Lbar = self.L0 + self.q*self.feed
-        self.Vbar = self.V-self.feed*(1-self.q)
-        self.BoilupRatio = self.Vbar/self.bottoms
-        print(self.BoilupRatio,self.V,self.distillate)
+        self.BoilupRatio = self.V[-1] / self.bottoms
+        self.stripping_line = [self.R / (1 + self.R) * x / 100
+                                     + self.db_fracs[0] / (self.R + 1) for x in range(1, 101)]
+        self.rectifying_line = [(self.BoilupRatio + 1) / self.BoilupRatio * x / 100
+                                      - self.db_fracs[1] / self.BoilupRatio for x in range(1, 101)]
+        operating_lines = [self.stripping_line,self.rectifying_line]
+        if len(self.L) and len(self.V) > 2:
+            for L,V in self.L[1:-1],self.V[1:-1]:
+                pass
+        for op_line in operating_lines:
+            if operating_lines[0] == op_line[0]:
+                self.find_operating_line_intersection(op_line,self.feed_line[0])
+            elif operating_lines[-1] == op_line[0]:
+                self.find_operating_line_intersection(op_line, self.feed_line[-1])
 
-        self.q_line = array([self.q / (self.q - 1) * x / 100
-                       - (self.feed_comp / (self.q - 1)) for x in range(1, 101)])
-
-        self.eqint=self.find_feed_equilibrium_intersection()
-        print(self.eqint)
 
 
-        self.stripping_line = array([self.R/(1+self.R)*x/100
-                               + self.outfracs[0]/(self.R+1) for x in range(1,101)])
-        self.rectifying_line = array([(self.BoilupRatio+1)/self.BoilupRatio*x/100
-                                - self.outfracs[1]/self.BoilupRatio for x in range(1, 101)])
 
-        self.intersectb = self.find_operating_line_intersection(self.rectifying_line, self.q_line)
-        self.intersectt = self.find_operating_line_intersection(self.stripping_line,self.q_line)
-    def find_feed_equilibrium_intersection(self):
-        for i in range(1,1001):
-            i = i/1000
-            q_cord = interp(i,self.line45,self.q_line)
-            eqcord = interp(i,self.xydata[0],self.xydata[1])
-            print(abs(q_cord-eqcord))
-            if abs(q_cord-eqcord) < 0.005:
-                x = interp(q_cord,self.xydata[1],xydata[0])
-                return x,q_cord
+    def find_feed_lines(self):
+        self.feed_equilibrium_intersect = array([[1,1]])
+        for _,zF,q in self.feed_flow_composition_and_state:
+            q_line = array([q/ (q - 1) * x / 100
+             - (zF / (q - 1)) for x in range(1, 101)])
+            for i in range(1,1001):
+                i = i/1000
+                q_cord = interp(i,self.line45,q_line)
+                eqcord = interp(i,self.xydata[0],self.xydata[1])
+                print(abs(q_cord-eqcord))
+                if abs(q_cord-eqcord) < 0.005:
+                    x = interp(q_cord,self.xydata[1],xydata[0])
+                    self.feed_equilibrium_intersect = append(self.feed_equilibrium_intersect,array([[x, eqcord]]),axis = 0)
+                    break
+            self.feed_line.append(q_line)
+        self.feed_equilibrium_intersect = delete(self.feed_equilibrium_intersect,0)
+        self.feed_equilibrium_intersect = delete(self.feed_equilibrium_intersect, 0)
 
     def find_operating_line_intersection(self,operating_line_1,feed_line):
+
         for i in range(1, 1001):
             i = i / 1000
             line1cord = interp(i, self.line45, operating_line_1)
@@ -98,9 +130,8 @@ class MTMethod():
         self.tray_finder()
         plot.plot(self.line45,self.line45)
         plot.plot([self.feed_comp,self.eqint[0]],[self.feed_comp,self.eqint[1]])
-        plot.plot([self.bottoms_fraction,self.intersectb[0]],[self.bottoms_fraction,self.intersectb[1]])
-        plot.plot([self.intersectt[0], self.distillate_fraction], [self.intersectt[1], self.distillate_fraction])
-        #plot.plot(self.line45,self.stripping_line)
+        plot.plot([self.dbfracs[1],self.intersectb[0]],[self.dbfracs[1],self.intersectb[1]])
+        plot.plot([self.intersectt[0], self.dbfracs[0]], [self.intersectt[1], self.dbfracs[0]])
         plot.plot(self.xydata[0],self.xydata[1])
         plot.show()
 
@@ -137,12 +168,12 @@ xydata = [list(x) for x in zip
 [0.97070, 	0.97150],
 [0.98250, 	0.98350]])]
 
-distillate_fraction = 0.8
+distillate_fraction = 0.85
 bottoms_fraction = 0.05
-feed = 100
-feed_composition = 0.6
-liquid_composition = 0.5
-relfux_ratio = 2
+feed = [100]
+feed_composition = [0.6]
+liquid_composition = [0.5]
+relfux_ratio = 1.6
 print(xydata[0])
 test = MTMethod(xydata[0],xydata[1],distillate_fraction,bottoms_fraction,feed,feed_composition,liquid_composition,relfux_ratio)
 test.find_operating_lines()
